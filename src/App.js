afc513b0cda97ea7c074a252b484eeb1414250cb
anthonychen1109 <chen.anthony.1109@gmail.com>
2017-08-16 23:45:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Cell from './cell/cell';
+import Cell from './Cell';
 
 class App extends Component {
+
+  playerTurn() {
+    this.setState({totalMoves: ++this.state.totalMoves});
+    if(this.state.player === 'X') {
+      this.setState({player: 'O'})
+    } else if(this.state.player === 'O') {
+      this.setState({player: 'X'})
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalMoves: 0,
+      active: false,
+      buttonText: '',
+      player: 'X'
+    };
+    this.playerTurn = this.playerTurn.bind(this);
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title">
           <h1>Tic Tac Toe</h1>
-          <div className="row grid">
-            <Cell />
-            <Cell />
-            <Cell />
+          <h2>Player {this.state.player}'s Turn</h2>
+        </div>
+        <div className="container">
+          <div className="row">
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
           </div>
-          <div className="row grid">
-            <Cell />
-            <Cell />
-            <Cell />
+          <div className="row">
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
           </div>
-          <div className="row grid">
-            <Cell />
-            <Cell />
-            <Cell />
+          <div className="row">
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
+            <Cell totalMoves={this.state.totalMoves} playerTurn={this.playerTurn} active={this.state.active} buttonText={this.state.buttonText}/>
           </div>
         </div>
       </div>
     );
   }
 }
+
 export default App;
